74c8f86ee4df8c456042c6f01cb6362b9018f5ec
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-12 13:51:05 -0300

    Adds solution to xxor

---
+++ Matemática/Lógica_Proposicional_Booleana/xxor.cpp
@@ -0,0 +1,57 @@
+// https://atcoder.jp/contests/abc117/tasks/abc117_d
+
+/*
+The middle of adventure, such a perfect place to start
+⣿⣿⣿⣿⣿⣿⣿⢿⠟⢿⣿⣿⣿⣿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠛⣿⢿⣿⣿⣿⣿
+⣿⣿⣿⡿⢿⣿⣿⣄⠄⣼⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠛⣿⣿⠄⢀⣤⣿⣿⣿⣿
+⣿⣿⣷⣤⣸⣿⠛⡛⢛⣿⠋⠄⠄⢀⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠁⠄⠁⠈⠄⠄⠄⠄⣿⡀⠄⠄⠄⠄⠄⠄⠄⠓⠸⠏⠄⢹⣿⣿⣿
+⣿⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⢠⠿⠿⢻⣦⣤⣠⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿
+⣿⣿⡿⠁⠄⠄⠄⠄⠈⠄⠄⢸⣿⣿⣿⣿⣿⢭⡛⡀⠄⠄⠨⠄⠄⠄⠄⠄⣿⣿
+⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣧⡀⣈⣿⣿⠟⠄⠄⠠⢇⠄⠄⠄⠄⠄⣿⣿
+⣿⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢀⣉⡉⠉⠄⠄⠄⠄⠄⠰⠂⠄⠄⠄⠄⢹⣿
+⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿
+⣿⣿⣶⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define ff first
+#define ss second
+#define ll long long
+#define int long long
+
+using namespace std;
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int n, k; cin >> n >> k;
+  int arr[n];
+
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  int x = 0;
+  for (int i = 62; i >= 0; i--) {
+    int curr = 0;
+    for (int j = 0; j < n; j++) {
+      if (arr[j] & (1LL << i)) curr++;
+    }
+
+    if (curr < n - curr && (x | (1LL << i)) <= k) x |= (1LL << i);
+  }
+
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += (arr[i] ^ x);
+  }
+
+  cout << sum << '\n';
+
+  return 0;
+}
