adbe54781f1017c4f58a4e893313c5dfc34672a2
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-12 22:41:52 -0300

    Adds solution to Calculation Function

---
+++ Matemática/Funções/calculating_function.cpp
@@ -0,0 +1,41 @@
+// https://codeforces.com/problemset/problem/486/A
+
+/*
+The middle of adventure, such a perfect place to start
+⣿⣿⣿⣿⣿⣿⣿⢿⠟⢿⣿⣿⣿⣿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠛⣿⢿⣿⣿⣿⣿
+⣿⣿⣿⡿⢿⣿⣿⣄⠄⣼⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠛⣿⣿⠄⢀⣤⣿⣿⣿⣿
+⣿⣿⣷⣤⣸⣿⠛⡛⢛⣿⠋⠄⠄⢀⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠁⠄⠁⠈⠄⠄⠄⠄⣿⡀⠄⠄⠄⠄⠄⠄⠄⠓⠸⠏⠄⢹⣿⣿⣿
+⣿⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⢠⠿⠿⢻⣦⣤⣠⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿
+⣿⣿⡿⠁⠄⠄⠄⠄⠈⠄⠄⢸⣿⣿⣿⣿⣿⢭⡛⡀⠄⠄⠨⠄⠄⠄⠄⠄⣿⣿
+⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣧⡀⣈⣿⣿⠟⠄⠄⠠⢇⠄⠄⠄⠄⠄⣿⣿
+⣿⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢀⣉⡉⠉⠄⠄⠄⠄⠄⠰⠂⠄⠄⠄⠄⢹⣿
+⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿
+⣿⣿⣶⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define ff first
+#define ss second
+#define ll long long
+#define int long long
+
+using namespace std;
+
+long long division_ceil(long long a, long long b) {
+    return 1 + ((a - 1) / b);
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  ll n; cin >> n;
+
+  cout << division_ceil(n, 2) * (n % 2 == 1 ? -1 : 1) << '\n';
+
+  return 0;
+}
