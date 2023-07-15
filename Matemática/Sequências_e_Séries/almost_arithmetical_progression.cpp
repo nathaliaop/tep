c07fa17ef7fea1086192f1e639084851ba234021
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-14 23:32:12 -0300

    Adds solution to Almost Arithmetical Progression

---
+++ Matemática/Sequências_e_Séries/almost_arithmetical_progression.cpp
@@ -0,0 +1,74 @@
+// https://codeforces.com/problemset/problem/255/C
+
+/*
+The middle of adventure, such a perfect place to start
+⣿⣿⣿⣿⣿⣿⣿⢿⠟⢿⣿⣿⣿⣿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠛⣿⢿⣿⣿⣿⣿
+⣿⣿⣿⡿⢿⣿⣿⣄⠄⣼⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠛⣿⣿⠄⢀⣤⣿⣿⣿⣿
+⣿⣿⣷⣤⣸⣿⠛⡛⢛⣿⠋⠄⠄⢀⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠁⠄⠁⠈⠄⠄⠄⠄⣿⡀⠄⠄⠄⠄⠄⠄⠄⠓⠸⠏⠄⢹⣿⣿⣿
+⣿⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⢠⠿⠿⢻⣦⣤⣠⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿
+⣿⣿⡿⠁⠄⠄⠄⠄⠈⠄⠄⢸⣿⣿⣿⣿⣿⢭⡛⡀⠄⠄⠨⠄⠄⠄⠄⠄⣿⣿
+⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣧⡀⣈⣿⣿⠟⠄⠄⠠⢇⠄⠄⠄⠄⠄⣿⣿
+⣿⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢀⣉⡉⠉⠄⠄⠄⠄⠄⠰⠂⠄⠄⠄⠄⢹⣿
+⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿
+⣿⣿⣶⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define ff first
+#define ss second
+#define ll long long
+#define int long long
+
+// 10, 20, 30, 20, 30
+
+// dp[0][10] = 1  ... 
+
+// dp[1][10] = max(1 + dp[1][20]);
+
+using namespace std;
+
+const int MAX = 4010;
+
+int n;
+int arr[MAX];
+int dp[MAX][MAX];
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  cin >> n;
+  map<int, int> m;
+  int curr = 0;
+  for (int i = 0, a; i < n; i++) {
+    cin >> a;
+    if (m.find(a) == m.end()) {
+      m[a] = curr++;
+    }
+    arr[i] = m[a];
+  }
+  
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      dp[i][arr[j]] = 1;
+    }
+  }
+
+  // dp[i[j] = > elemento terminando na posição i com o elemento anterior sendo j
+  int mx = 1;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      dp[i][arr[j]] = max(dp[i][arr[j]], dp[j][arr[i]] + 1);
+      mx = max(mx, dp[i][arr[j]]);
+    }
+  }
+  
+  cout << mx << '\n';
+
+  return 0;
+}
+
