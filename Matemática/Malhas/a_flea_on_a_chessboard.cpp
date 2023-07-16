8665fc8fe42da84b99b632ddbdebbf4e23af5642
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-15 23:19:58 -0300

    Adds solution to A Flea on a Chessboard

---
+++ Matemática/Malhas/a_flea_on_a_chessboard.cpp
@@ -0,0 +1,65 @@
+// https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1561
+
+/*
+The middle of adventure, such a perfect place to start
+⣿⣿⣿⣿⣿⣿⣿⢿⠟⢿⣿⣿⣿⣿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠛⣿⢿⣿⣿⣿⣿
+⣿⣿⣿⡿⢿⣿⣿⣄⠄⣼⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠛⣿⣿⠄⢀⣤⣿⣿⣿⣿
+⣿⣿⣷⣤⣸⣿⠛⡛⢛⣿⠋⠄⠄⢀⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠁⠄⠁⠈⠄⠄⠄⠄⣿⡀⠄⠄⠄⠄⠄⠄⠄⠓⠸⠏⠄⢹⣿⣿⣿
+⣿⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⢠⠿⠿⢻⣦⣤⣠⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿
+⣿⣿⡿⠁⠄⠄⠄⠄⠈⠄⠄⢸⣿⣿⣿⣿⣿⢭⡛⡀⠄⠄⠨⠄⠄⠄⠄⠄⣿⣿
+⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣧⡀⣈⣿⣿⠟⠄⠄⠠⢇⠄⠄⠄⠄⠄⣿⣿
+⣿⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢀⣉⡉⠉⠄⠄⠄⠄⠄⠰⠂⠄⠄⠄⠄⢹⣿
+⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿
+⣿⣿⣶⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define ld long double
+#define ll long long
+#define int long long
+#define pii pair<int, int>
+
+using namespace std;
+
+// 12 22
+// 11 21
+// 10 20 30 40 50 60  
+// 1  2  3  4  5  6  
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int S, x, y, dx, dy; 
+  while (cin >> S >> x >> y >> dx >> dy) {
+    if (S == 0 && x == 0 && y == 0 && dx == 0 && dy == 0) break;
+
+    int jumps = 0;
+    
+    // if impossible
+
+    int n = 100000;
+    bool found = false;
+    while (n--) {
+      // white square
+      if (x % S != 0 && y % S != 0 && (x / S + y / S) % 2 == 1) {
+        found = true;
+        printf("After %lld jumps the flea lands at (%lld, %lld).\n", jumps, x, y);
+        break;
+      }
+
+      x += dx;
+      y += dy;
+      jumps++;
+    }
+
+    if (!found) printf("The flea cannot escape from black squares.\n");
+  }
+
+  return 0;
+}
