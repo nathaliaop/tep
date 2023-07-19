5d3b36c528ef27ed86c611722915e9e8bd8fc11c
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-19 14:04:58 -0300

    Adds solution to Digit Sum

---
+++ Matemática/Bases_Numéricas/Representação_Binária/digit_sum.cpp
@@ -0,0 +1,75 @@
+/*
+The middle of adventure, such a perfect place to start
+⣿⣿⣿⣿⣿⣿⣿⢿⠟⢿⣿⣿⣿⣿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠛⣿⢿⣿⣿⣿⣿
+⣿⣿⣿⡿⢿⣿⣿⣄⠄⣼⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠛⣿⣿⠄⢀⣤⣿⣿⣿⣿
+⣿⣿⣷⣤⣸⣿⠛⡛⢛⣿⠋⠄⠄⢀⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠁⠄⠁⠈⠄⠄⠄⠄⣿⡀⠄⠄⠄⠄⠄⠄⠄⠓⠸⠏⠄⢹⣿⣿⣿
+⣿⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⢠⠿⠿⢻⣦⣤⣠⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿
+⣿⣿⡿⠁⠄⠄⠄⠄⠈⠄⠄⢸⣿⣿⣿⣿⣿⢭⡛⡀⠄⠄⠨⠄⠄⠄⠄⠄⣿⣿
+⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣧⡀⣈⣿⣿⠟⠄⠄⠠⢇⠄⠄⠄⠄⠄⣿⣿
+⣿⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢀⣉⡉⠉⠄⠄⠄⠄⠄⠰⠂⠄⠄⠄⠄⢹⣿
+⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿
+⣿⣿⣶⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define ld long double
+#define ll long long
+#define int long long
+#define pii pair<int, int>
+
+using namespace std;
+
+const ll INF = (ll)1e18+10;
+
+int f (int b, int n) {
+  if (n < b) return n;
+  return f(b, n / b) + n % b;
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n, s; cin >> n >> s;
+
+  if (n < s) {
+    cout << "-1\n";
+    return 0;
+  }
+
+  if (n == s) {
+    cout << n + 1 << '\n';
+    return 0;
+  }
+
+  int ans = INF;
+  int sq = ceil(sqrt(n));
+  // b <= sqrt(n)
+  for (int b = 2; b <= sq; b++) {
+    if (f(b, n) == s) ans = min(ans, b);
+  }
+
+  // 2 algarismos p + q = s
+  // n = p * b + q
+  
+  // n - s = p * b + q - p - q
+  // n - s = p * b - p = p * (b - 1)
+  // ((n - s) / p) + 1 = b
+
+  // b >= sqrt(n), so p <= sqrt(n)
+  for (int p = 1; p <= sq; p++) {
+    int val = n - s;
+    if (val % p != 0) continue;
+    if (f(val / p + 1, n) == s) ans = min(ans, val / p + 1);
+  }
+
+  if (ans == INF) cout << "-1\n";
+  else cout << ans << '\n';
+
+  return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Resolução dos problemas do [repositório da disciplina de Tópicos Especiais em Programação](https://github.com/edsomjr/TEP/tree/master)
