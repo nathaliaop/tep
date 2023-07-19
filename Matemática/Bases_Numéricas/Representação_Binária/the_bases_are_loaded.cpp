44ff27bb1d66442f925c020844049a9a8f290001
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-19 17:17:53 -0300

    Adds solution to The Bases Are Loaded

---
+++ Matemática/Bases_Numéricas/Representação_Binária/the_bases_are_loaded.cpp
@@ -0,0 +1,72 @@
+/*
+The middle of adventure, such a perfect place to start
+⣿⣿⣿⣿⣿⣿⣿⢿⠟⢿⣿⣿⣿⣿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠛⣿⢿⣿⣿⣿⣿
+⣿⣿⣿⡿⢿⣿⣿⣄⠄⣼⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠛⣿⣿⠄⢀⣤⣿⣿⣿⣿
+⣿⣿⣷⣤⣸⣿⠛⡛⢛⣿⠋⠄⠄⢀⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠁⠄⠁⠈⠄⠄⠄⠄⣿⡀⠄⠄⠄⠄⠄⠄⠄⠓⠸⠏⠄⢹⣿⣿⣿
+⣿⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⢠⠿⠿⢻⣦⣤⣠⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿
+⣿⣿⡿⠁⠄⠄⠄⠄⠈⠄⠄⢸⣿⣿⣿⣿⣿⢭⡛⡀⠄⠄⠨⠄⠄⠄⠄⠄⣿⣿
+⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣧⡀⣈⣿⣿⠟⠄⠄⠠⢇⠄⠄⠄⠄⠄⣿⣿
+⣿⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢀⣉⡉⠉⠄⠄⠄⠄⠄⠰⠂⠄⠄⠄⠄⢹⣿
+⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿
+⣿⣿⣶⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define ld long double
+#define ll long long
+#define int long long
+#define pii pair<int, int>
+
+using namespace std;
+
+const string digits { "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+
+string representation(int n, int b) {
+  string rep;
+
+  do {
+    rep.push_back(digits[n % b]);
+    n /= b;
+  } while (n);
+
+  reverse(rep.begin(), rep.end());
+
+  return rep;
+}
+
+long long to_decimal(const string& rep, long long base) {
+  long long n = 0;
+
+  for (auto c : rep) {
+    // if the number can't be represented in this base
+    if (c > digits[base - 1]) return -1;
+    n *= base;
+    n += digits.find(c);
+  }
+
+  return n;
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int b1, b2; string num;
+  while (cin >> b1 >> b2 >> num) {
+    ll dec = to_decimal(num, b1);
+    if (dec == -1) {
+      cout << num << " is an illegal base " << b1 << " number\n";
+      continue;
+    }
+    string rep = representation(dec, b2);
+    cout << num << " base " << b1 << " = " << rep << " base " << b2 << '\n';
+  }
+
+  return 0;
+}
+
