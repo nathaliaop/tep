188d57028dc5d934a98755726d57d2df89354bc7
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-19 14:18:50 -0300

    Adds solution to Little Elephant and Bits

---
+++ Matemática/Bases_Numéricas/Representação_Binária/little_elephant_ad_bits.cpp
@@ -0,0 +1,41 @@
+/*
+The middle of adventure, such a perfect place to start
+⣿⣿⣿⣿⣿⣿⣿⢿⠟⢿⣿⣿⣿⣿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠛⣿⢿⣿⣿⣿⣿
+⣿⣿⣿⡿⢿⣿⣿⣄⠄⣼⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠛⣿⣿⠄⢀⣤⣿⣿⣿⣿
+⣿⣿⣷⣤⣸⣿⠛⡛⢛⣿⠋⠄⠄⢀⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠁⠄⠁⠈⠄⠄⠄⠄⣿⡀⠄⠄⠄⠄⠄⠄⠄⠓⠸⠏⠄⢹⣿⣿⣿
+⣿⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⢠⠿⠿⢻⣦⣤⣠⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿
+⣿⣿⡿⠁⠄⠄⠄⠄⠈⠄⠄⢸⣿⣿⣿⣿⣿⢭⡛⡀⠄⠄⠨⠄⠄⠄⠄⠄⣿⣿
+⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣧⡀⣈⣿⣿⠟⠄⠄⠠⢇⠄⠄⠄⠄⠄⣿⣿
+⣿⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢀⣉⡉⠉⠄⠄⠄⠄⠄⠰⠂⠄⠄⠄⠄⢹⣿
+⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿
+⣿⣿⣶⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define ld long double
+#define ll long long
+#define int long long
+#define pii pair<int, int>
+
+using namespace std;
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  string s; cin >> s;
+  bool del = false;
+  int sz = s.size();
+  for (int i = 0; i < sz; i++) {
+    if ((s[i] == '0' || i == sz - 1) && !del) del =true;
+    else cout << s[i];
+  }
+  cout << '\n';
+
+  return 0;
+}
