c468cc2ea8c5460f461998f22942e1685d91a3df
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-20 11:37:19 -0300

    Adds solution to Neko Performs Cat Furrier Transform

---
+++ Matemática/Bases_Numéricas/Operações_Binárias/neko_performs_cat_furrier_transform.cpp
@@ -0,0 +1,68 @@
+/*
+The middle of adventure, such a perfect place to start
+⣿⣿⣿⣿⣿⣿⣿⢿⠟⢿⣿⣿⣿⣿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠛⣿⢿⣿⣿⣿⣿
+⣿⣿⣿⡿⢿⣿⣿⣄⠄⣼⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠛⣿⣿⠄⢀⣤⣿⣿⣿⣿
+⣿⣿⣷⣤⣸⣿⠛⡛⢛⣿⠋⠄⠄⢀⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠁⠄⠁⠈⠄⠄⠄⠄⣿⡀⠄⠄⠄⠄⠄⠄⠄⠓⠸⠏⠄⢹⣿⣿⣿
+⣿⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⢠⠿⠿⢻⣦⣤⣠⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿
+⣿⣿⡿⠁⠄⠄⠄⠄⠈⠄⠄⢸⣿⣿⣿⣿⣿⢭⡛⡀⠄⠄⠨⠄⠄⠄⠄⠄⣿⣿
+⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣧⡀⣈⣿⣿⠟⠄⠄⠠⢇⠄⠄⠄⠄⠄⣿⣿
+⣿⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢀⣉⡉⠉⠄⠄⠄⠄⠄⠰⠂⠄⠄⠄⠄⢹⣿
+⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿
+⣿⣿⣶⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define ld long double
+#define ll long long
+#define pii pair<int, int>
+#define msb(x) 31 - __builtin_clz(x)
+
+using namespace std;
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int n; cin >> n;
+  vector<int> ans;
+  int count = 0;
+
+  while (true) {
+    bool found = false;
+    for (int i = msb(n); i >= 0; i--) {
+        if (!(n & (1 << i))) {
+          found = true;
+          break;
+        }
+    }
+    if (!found) break;
+    if (count != 0) {
+      n++;
+      count++;
+    }
+    found = false;
+    for (int i = msb(n); i >= 0; i--) {
+      if (!(n & (1 << i))) {
+        ans.pb(i + 1);
+        found = true;
+        n ^= (1 << (i + 1)) - 1;
+        count++;
+        break;
+      } 
+    }
+    if (!found) break;
+  }
+
+  cout << count << '\n';
+  for (auto e : ans) {
+    cout << e << ' ';
+  }
+  if (count !=0) cout << '\n';
+
+  return 0;
+}
