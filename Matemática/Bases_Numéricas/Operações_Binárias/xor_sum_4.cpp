0467ef73544619dfa3551c94457d454d4fc91194
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-20 19:42:27 -0300

    Adds solution to XOR Sum 4

---
+++ Matemática/Bases_Numéricas/Operações_Binárias/xor_sum_4.cpp
@@ -0,0 +1,69 @@
+/*
+The middle of adventure, such a perfect place to start
+⣿⣿⣿⣿⣿⣿⣿⢿⠟⢿⣿⣿⣿⣿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠛⣿⢿⣿⣿⣿⣿
+⣿⣿⣿⡿⢿⣿⣿⣄⠄⣼⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠛⣿⣿⠄⢀⣤⣿⣿⣿⣿
+⣿⣿⣷⣤⣸⣿⠛⡛⢛⣿⠋⠄⠄⢀⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠁⠄⠁⠈⠄⠄⠄⠄⣿⡀⠄⠄⠄⠄⠄⠄⠄⠓⠸⠏⠄⢹⣿⣿⣿
+⣿⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⢠⠿⠿⢻⣦⣤⣠⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿
+⣿⣿⡿⠁⠄⠄⠄⠄⠈⠄⠄⢸⣿⣿⣿⣿⣿⢭⡛⡀⠄⠄⠨⠄⠄⠄⠄⠄⣿⣿
+⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣧⡀⣈⣿⣿⠟⠄⠄⠠⢇⠄⠄⠄⠄⠄⣿⣿
+⣿⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢀⣉⡉⠉⠄⠄⠄⠄⠄⠰⠂⠄⠄⠄⠄⢹⣿
+⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿
+⣿⣿⣶⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define ld long double
+#define ll long long
+#define int long long
+#define pii pair<int, int>
+
+using namespace std;
+
+const int BITS = 63;
+const int MOD = 1000000007;
+
+ll fexp(ll b, ll e, ll mod) {
+    ll res = 1;
+    b %= mod;
+    while(e){ 
+        if(e & 1LL)
+            res = (res * b) % mod;
+        e = e >> 1LL;
+        b = (b * b) % mod;
+    }
+    return res;
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int n; cin >> n;
+  int a[n];
+
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  int ans = 0;
+  for (int j = BITS; j >= 0; j--) {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+      if (a[i] & (1LL << j)) count++;
+    }
+
+    int tmp = count * (n - count);
+    tmp %= MOD;
+    ans += tmp * fexp(2, j, MOD);
+    ans %= MOD;
+  }
+
+  cout << ans % MOD << '\n';
+
+  return 0;
+}
