92022dbd7c7c58ca21d618ef12231aa05de4c0e6
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-19 23:15:50 -0300

    Adds solution to Two Sequences

---
+++ Matemática/Bases_Numéricas/Operações_Binárias/two_sequences.cpp
@@ -0,0 +1,62 @@
+/*
+The middle of adventure, such a perfect place to start
+⣿⣿⣿⣿⣿⣿⣿⢿⠟⢿⣿⣿⣿⣿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠛⣿⢿⣿⣿⣿⣿
+⣿⣿⣿⡿⢿⣿⣿⣄⠄⣼⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠛⣿⣿⠄⢀⣤⣿⣿⣿⣿
+⣿⣿⣷⣤⣸⣿⠛⡛⢛⣿⠋⠄⠄⢀⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠁⠄⠁⠈⠄⠄⠄⠄⣿⡀⠄⠄⠄⠄⠄⠄⠄⠓⠸⠏⠄⢹⣿⣿⣿
+⣿⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⢠⠿⠿⢻⣦⣤⣠⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿
+⣿⣿⡿⠁⠄⠄⠄⠄⠈⠄⠄⢸⣿⣿⣿⣿⣿⢭⡛⡀⠄⠄⠨⠄⠄⠄⠄⠄⣿⣿
+⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣧⡀⣈⣿⣿⠟⠄⠄⠠⢇⠄⠄⠄⠄⠄⣿⣿
+⣿⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢀⣉⡉⠉⠄⠄⠄⠄⠄⠰⠂⠄⠄⠄⠄⢹⣿
+⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿
+⣿⣿⣶⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿
+*/
+#include <algorithm>
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define ld long double
+#define ll long long
+#define int long long
+#define pii pair<int, int>
+
+using namespace std;
+
+const int BITS = 28;
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+ 
+  int n; cin >> n;
+  int a[n], b[n];
+
+  for (int i = 0; i < n; i++) cin >> a[i];
+  for (int i = 0; i < n; i++) cin >> b[i];
+
+  int ans = 0;
+
+  for (int i = BITS; i >= 0; i--) {
+    for (int j = 0; j < n; j++) {
+      a[j] %= (1 << (i + 1));
+      b[j] %= (1 << (i + 1));
+    }
+    
+    sort(b, b + n);
+
+    int count = 0;
+    for (int j = 0; j < n; j++) {
+      count += lower_bound(b, b + n, (1 << (i + 1)) - a[j]) - lower_bound(b, b + n, (1 << i) - a[j]);
+      count += lower_bound(b, b + n, (1 << (i + 2)) - a[j]) - lower_bound(b, b + n, (1 << i) + (1 << (i + 1)) - a[j]);
+    }
+    
+    if (count % 2 == 1) ans |= (1 << i);
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
