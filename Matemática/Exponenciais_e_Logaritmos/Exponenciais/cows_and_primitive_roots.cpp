c3d281b84c9aa164f783880fcab9e74dd35da11b
Nathália Pereira <np.nathaliapereira@gmail.com>
2023-07-20 21:52:26 -0300

    Adds solution to Cows and Primitive Roots

---
+++ Matemática/Exponenciais_e_Logaritmos/Exponenciais/cows_and_primitive_roots.cpp
@@ -0,0 +1,58 @@
+/*
+The middle of adventure, such a perfect place to start
+⣿⣿⣿⣿⣿⣿⣿⢿⠟⢿⣿⣿⣿⣿⠿⠛⠛⠿⢿⣿⣿⣿⣿⠛⣿⢿⣿⣿⣿⣿
+⣿⣿⣿⡿⢿⣿⣿⣄⠄⣼⣿⡿⠋⠄⠄⠄⠄⠄⠄⠄⠛⣿⣿⠄⢀⣤⣿⣿⣿⣿
+⣿⣿⣷⣤⣸⣿⠛⡛⢛⣿⠋⠄⠄⢀⠄⠄⠄⠄⠄⠄⠄⠘⣿⣿⡿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⡿⠁⠄⠁⠈⠄⠄⠄⠄⣿⡀⠄⠄⠄⠄⠄⠄⠄⠓⠸⠏⠄⢹⣿⣿⣿
+⣿⣿⣿⡿⠄⠄⠄⠄⠄⠄⠄⢠⠿⠿⢻⣦⣤⣠⠄⠄⠄⠄⠄⠄⠄⠄⠈⠻⣿⣿
+⣿⣿⡿⠁⠄⠄⠄⠄⠈⠄⠄⢸⣿⣿⣿⣿⣿⢭⡛⡀⠄⠄⠨⠄⠄⠄⠄⠄⣿⣿
+⣿⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣧⡀⣈⣿⣿⠟⠄⠄⠠⢇⠄⠄⠄⠄⠄⣿⣿
+⣿⡁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢀⢀⣉⡉⠉⠄⠄⠄⠄⠄⠰⠂⠄⠄⠄⠄⢹⣿
+⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⠿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⣿
+⣿⣿⣶⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿
+*/
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define ld long double
+#define ll long long
+#define int long long
+#define pii pair<int, int>
+
+using namespace std;
+
+ll fexp(ll b, ll e, ll mod) {
+    ll res = 1;
+    b %= mod;
+    while(e){ 
+        if(e & 1LL)
+            res = (res * b) % mod;
+        e = e >> 1LL;
+        b = (b * b) % mod;
+    }
+    return res;
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int p; cin >> p;
+
+  int ans = 0;
+  for (int x = 1; x <= p; x++) {
+    bool valid = true;
+    for (int e = 1; e <= p - 2; e++) {
+      if ((fexp(x, e, p) - 1) % p == 0) valid = false;
+    }
+    if ((fexp(x, p - 1, p) - 1) % p != 0) valid = false;
+    if (valid) ans++;
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
